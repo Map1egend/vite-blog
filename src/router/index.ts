ba71c366e0f26e059ac728f65edc0386dcd27032
Maple <jellyfishtatta@icloud.com>
2022-08-24 22:30:26 +0800

    feat: Home  页面添加 svg 动画

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,8 @@
 import { RouteRecordRaw, createRouter, createWebHashHistory } from 'vue-router'
 
 const routes: Array<RouteRecordRaw> = [
-  { path: '/', component: () => import('../layouts/DrawBoard.vue') },
+  { path: '/', redirect: '/home' },
+  { path: '/drawboard', component: () => import('../layouts/DrawBoard.vue') },
   { path: '/home', component: () => import('@views/HomePage.vue') },
   { path: '/blog', component: () => import('@views/BlogPage.vue') }
 ]
